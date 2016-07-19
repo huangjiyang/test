34eec6039b4b55da699fed1c17cee8e79c97b0f4
huangjiyang <huangjiyangdp@sina.com>
2016-07-19 12:34:46 +0800

    project init

---
+++ git_learn/src/main/java/com/hjy/git/learn/App.java
@@ -0,0 +1,13 @@
+package com.hjy.git.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(" test git ");
+    }
+}
